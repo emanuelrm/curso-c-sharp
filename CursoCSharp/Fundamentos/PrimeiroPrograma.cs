89934de173e6af696ca33e676a4801ba09aa006f
emanuel <emanuel@DESKTOP-KQGNKSP>
2018-07-19 20:34:20 -0400

    '123123

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+
+        public static void Executar()
+        {
+            System.Console.Write("Primeiro");
+            System.Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }        
+    }
+}
---
+++ CursoCSharp/VariaveisEConstantes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            //area da circunferencia
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+            //PI = 3.1415;
+
+            double area = PI * raio * raio;
+            Console.WriteLine("Área é " + area);
+
+            //Tipos internos
+            bool estaChovendo = true;
+            Console.WriteLine("Está chovendo " + estaChovendo);
+
+            byte idade = 45;
+            Console.WriteLine("Idade " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de Gols " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salário " + salario);
+
+            int menorValorInt = int.MinValue; //Mais usados dos inteiros!
+            Console.WriteLine("Menor int " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("População Brasileria " + populacaoBrasileira);
+
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor long " + menorValorLong);
+
+            ulong populacaoMundial = 7_600_000_000;
+            Console.WriteLine("População Mundial " + populacaoMundial);
+
+            float precoComputador = 1299.99F;
+            Console.WriteLine("Preço Computador " + precoComputador);
+
+            double valorDeMercadoDaApple = 1_000_000_000.00; //Mais usado dos reais
+            Console.WriteLine("Valor Apple " + valorDeMercadoDaApple);
+
+            decimal distanciaEntreEstrelas = decimal.MaxValue;
+            Console.WriteLine("Distância entre Estrelas " + distanciaEntreEstrelas);
+
+            char letra = 'a'; // Precisa ter aspas simples
+            Console.WriteLine("Letra " + letra);
+
+            string texto = "Sejam bem vindo ao curso de C#";
+            Console.WriteLine(texto);
+        }
+    }
+}
